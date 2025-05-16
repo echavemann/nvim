2179226b963b462c78d37287bb4ecfa64a862f4f
Ethan Havemann <echavemann@gmail.com>
2025-05-16 16:10:54 -0500

    lsp binds

--- lua/echavemann/lazy/lsp.lua
+++ lua/echavemann/lazy/lsp.lua
@@ -5,6 +5,13 @@ return {
         "williamboman/mason-lspconfig.nvim",
     },
     config = function()
+        local function lsp_keymaps(bufnr)
+            local opts = { buffer = bufnr, noremap = true, silent = true }
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = 'Go to Definition' }))
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, vim.tbl_extend('force', opts, { desc = 'Go to Declaration' }))
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, vim.tbl_extend('force', opts, { desc = 'Find References' }))
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = 'Hover Documentation' }))
+        end
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
@@ -15,7 +22,12 @@ return {
             automatic_installation = true,
             handlers = {
                 function(server_name)
-                    require("lspconfig")[server_name].setup {}
+                    require("lspconfig")[server_name].setup {
+                        capabilities = require('blink.cmp').get_lsp_capabilities(),
+                        on_attach = function(client, bufnr)
+                            lsp_keymaps(bufnr)
+                        end,
+                    }
                 end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
@@ -32,6 +44,5 @@ return {
                 end
             }
         })
-
     end
 }
