df32a20446b66c3c31074674f686b07c4ef031e1
Ethan Havemann <echavemann@gmail.com>
2025-05-16 14:50:54 -0500

    >

--- lua/echavemann/lazy/init.lua
+++ lua/echavemann/lazy/init.lua
@@ -1,8 +1,10 @@
 return {
     {
-        "olimorris/onedarkpro.nvim",
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
-            vim.cmd("colorscheme onedark")
-        end
+            vim.cmd.colorscheme("tokyonight")
+        end,
     },
 }
